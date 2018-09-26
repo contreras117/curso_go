fad899ea7e3f18c4692631254ca1b5092d653b0e
Daniel Contreras <dcontrer@pros.com>
2018-09-26 17:22:28 -0600

    Methods

---
+++ structs/course/course.go
@@ -0,0 +1,19 @@
+package course
+
+import (
+	"fmt"
+)
+
+//Course - La palabra reservada struct no permite crear struct's, lo mas parecidos a clases dentro del leguaje GO.
+type Course struct {
+	Name   string
+	Slug   string
+	Skills []string
+}
+
+//Para crear funciones dentro que trabajen con una instancia de un struct (metodos), se crean como funciones colocando el struct que los va a utilizar antes del nombre de la funcion.
+
+//Suscribe imprime en pantalla un mensaje de que el usuario proprorcionado se ha inscrito al curso que llamo al metodo.
+func (c Course) Suscribe(name string) {
+	fmt.Printf("El usuario %s se ha inscrito al curso %s. Felicidades!!!", name, c.Name)
+}
--- structs/main.go
+++ structs/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	. "github.com/contreras117/gocurso/structs/course"
 )
 
 //MiDato - Con la palabra reservada type podemos crear nuevos tipos de datos a partir de otros.
 type MiDato int
-
-//Course - La palabra reservada struct no permite crear struct's, lo mas parecidos a clases dentro del leguaje GO.
-type Course struct {
-	name   string
-	slug   string
-	skills []string
-}
 
 //OtroCurso Poniendo un struct dentro de la declararion de otro, este ultimo hereda todos los campos del primero, algo muy parecido a herencia.
 type OtroCurso struct {
@@ -27,15 +22,15 @@ type Career struct {
 func main() {
 
 	//Esta es la manera larga y descriptiva de inicializar una instancia de struct, asignando sus valores desde un inicio.
-	curso := Course{name: "Progrmacion con go", slug: "go", skills: []string{"Backend", "Servidores", "Programacion"}}
+	curso := Course{Name: "Progrmacion con go", Slug: "go", Skills: []string{"Backend", "Servidores", "Programacion"}}
 	//Con la funcion new podemos inicializar un struct para despues asignarle los valores de sus elemetos. La diferencia es que la funcion new crea la variable como una apuntador del struct, en este caso *Course.
 	curso2 := new(Course)
-	curso2.name = "Bases de datos"
-	curso2.slug = "BDD"
-	curso2.skills = []string{"Backend", "servidores"}
+	curso2.Name = "Bases de datos"
+	curso2.Slug = "BDD"
+	curso2.Skills = []string{"Backend", "servidores"}
 
 	otro := new(OtroCurso)
-	otro.name = "Nombre"
+	otro.Name = "Nombre"
 
 	carrera := new(Career)
 
@@ -44,4 +39,6 @@ func main() {
 	fmt.Println(curso)
 	fmt.Println(curso2)
 	fmt.Println(carrera)
+
+	curso.Suscribe("Daniel")
 }
